ebe6794130d33466f4bfd17e0acab3d01249b35e
Rob Noble <robjohnnoble@gmail.com>
2018-04-03 17:19:51 +0200

    Add data collation / summarising functions

---
+++ R/parameters-data.R
@@ -0,0 +1,12 @@
+#' Example parameters file
+#'
+#' Values of simulation parameters.
+#'
+#' @docType data
+#'
+#' @usage data(parameters)
+#'
+#' @format Dataframe with one row containing parameter values, and column names corresponding to parameter names.
+#'
+#' @keywords datasets
+"parameters"
