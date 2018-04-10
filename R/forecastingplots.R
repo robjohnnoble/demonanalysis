567894714c6e729246db2f915f7ab9a82b5c3ee1
Rob Noble <robjohnnoble@gmail.com>
2018-04-10 11:54:20 +0200

    Fix error in last edit; more forecastingplots

--- R/forecastingplots.R
+++ R/forecastingplots.R
@@ -1,15 +1,26 @@
-# outcome/waiting time versus diversity, coloured by K (JohnCode1)
-# may need to cut out the data from after when tumours start to reach the max size
-plot_outcome_versus_diversity <- function(cor_summary, output_filename = NA, file_type = "png", output_dir = NA) {
+#' Plot outcome versus diversity, coloured by K
+#' 
+#' @param df data frame
+#' @param output_dir folder in which to save the image file; if NA then plots are displayed on screen instead
+#' @param output_filename name of output image file
+#' @param file_type either "pdf" or "png" (other values default to "pdf")
+#' 
+#' @return a plot object
+#' 
+#' @export
+#' @import dplyr
+#' @importFrom graphics abline
+#' @importFrom graphics legend
+plot_outcome_versus_diversity <- function(df, output_filename = NA, file_type = "png", output_dir = NA) {
   if(!is.na(output_filename)) if(substr(output_dir, nchar(output_dir), nchar(output_dir)) != "/") output_dir <- paste0(output_dir, "/")
   
   if(!is.na(output_dir)) {
-    if(file_type == "png") png(paste0(output_dir,output_filename,".png"), width = 1000, height = 1000, res = 100)
-    else pdf(paste0(output_dir,output_filename,".pdf"), width = 500, height = 600)
+    if(file_type == "png") png(paste0(output_dir, output_filename,".png"), width = 1000, height = 1000, res = 100)
+    else pdf(paste0(output_dir, output_filename,".pdf"), width = 500, height = 600)
   }
   
-  start_size_range <- unique(cor_summary$start_size)
-  K_range <- unique(cor_summary$K)
+  start_size_range <- unique(df$start_size)
+  K_range <- unique(df$K)
   
   cols <- rainbow(8)
   cols[5] <- "blue"
@@ -25,7 +36,7 @@ plot_outcome_versus_diversity <- function(cor_summary, output_filename = NA, fil
         if(start_size_val == start_size_range[1]) legend("topright", as.character(K_range), title = "K", 
                                                      xpd = TRUE, horiz = FALSE, inset = c(0.1, 0.03), bty = "n", lty = 1, col = cols)
       }
-      lines(Cor_DriverDiversity ~ gap, data = filter(cor_summary, start_size == start_size_val, K == K_val), 
+      lines(Cor_DriverDiversity ~ gap, data = filter(df, start_size == start_size_val, K == K_val), 
             col = cols[log2(K) + 1], lwd = 2)
       abline(h = 0, untf = FALSE, lty = 3)
     }
@@ -34,17 +45,29 @@ plot_outcome_versus_diversity <- function(cor_summary, output_filename = NA, fil
   if(!is.na(output_dir)) dev.off()
 }
 
-# trajectories, coloured by K (JohnCode1)
-plot_outcome_versus_waiting_time <- function(cor_summary, output_filename = NA, file_type = "png", output_dir = NA) {
+#' Plot outcome versus waiting time, coloured by K
+#' 
+#' @param df data frame
+#' @param output_dir folder in which to save the image file; if NA then plots are displayed on screen instead
+#' @param output_filename name of output image file
+#' @param file_type either "pdf" or "png" (other values default to "pdf")
+#' 
+#' @return a plot object
+#' 
+#' @export
+#' @import dplyr
+#' @importFrom grDevices rainbow
+#' @importFrom graphics legend
+plot_outcome_versus_waiting_time <- function(df, output_filename = NA, file_type = "png", output_dir = NA) {
   if(!is.na(output_filename)) if(substr(output_dir, nchar(output_dir), nchar(output_dir)) != "/") output_dir <- paste0(output_dir, "/")
   
   if(!is.na(output_dir)) {
-    if(file_type == "png") png(paste0(output_dir,output_filename,".png"), width = 1000, height = 1000, res = 100)
-    else pdf(paste0(output_dir,output_filename,".pdf"), width = 500, height = 600)
+    if(file_type == "png") png(paste0(output_dir, output_filename, ".png"), width = 1000, height = 1000, res = 100)
+    else pdf(paste0(output_dir, output_filename, ".pdf"), width = 500, height = 600)
     }
   
-  start_size_range <- unique(cor_summary$start_size)
-  K_range <- unique(cor_summary$K)
+  start_size_range <- unique(df$start_size)
+  K_range <- unique(df$K)
   
   cols <- rainbow(8)
   cols[5] <- "blue"
@@ -58,7 +81,7 @@ plot_outcome_versus_waiting_time <- function(cor_summary, output_filename = NA, 
       legend("topright", as.character(K_range), title = "K", 
              xpd = TRUE, horiz = FALSE, inset = c(0.1, 0.03), bty = "n", lty = 1, col = cols)
       }
-      lines(Cor_DriverDiversity ~ start_size, data = filter(cor_summary, K == K_val), 
+      lines(Cor_DriverDiversity ~ start_size, data = filter(df, K == K_val), 
             col = cols[log2(K) + 1], lwd = 2)
       abline(h = 0, untf = FALSE, lty = 3)
       }
@@ -66,28 +89,69 @@ plot_outcome_versus_waiting_time <- function(cor_summary, output_filename = NA, 
   if(!is.na(output_dir)) dev.off()
 }
 
-# trajectories (relative time) coloured by K:
-qplot(
-  gen_adj,
-  NumCells,
-  group = interaction(K, seed),
-  data = data,
-  colour = factor(K),
-  geom = "line")
+#' Plot growth trajectories, coloured by K
+#' 
+#' @param df data frame
+#' @param output_dir folder in which to save the image file; if NA then plots are displayed on screen instead
+#' @param output_filename name of output image file
+#' @param file_type either "pdf" or "png" (other values default to "pdf")
+#' 
+#' @return a plot object
+#' 
+#' @export
+#' @import ggplot2
+plot_trajectories_by_K <- function(df, output_filename = NA, file_type = "png", output_dir = NA) {
+  if(!is.na(output_filename)) if(substr(output_dir, nchar(output_dir), nchar(output_dir)) != "/") output_dir <- paste0(output_dir, "/")
+  
+  if(!is.na(output_dir)) {
+    if(file_type == "png") png(paste0(output_dir, output_filename, ".png"), width = 1000, height = 1000, res = 100)
+    else pdf(paste0(output_dir, output_filename, ".pdf"), width = 500, height = 600)
+  }
+  
+  qplot(
+    gen_adj,
+    NumCells,
+    group = interaction(K, seed),
+    data = df,
+    colour = factor(K),
+    geom = "line")
+  
+  if(!is.na(output_dir)) dev.off()
+}
 
-# time adjusted trajectories by driver edge diversity (JohnCode1)
-qp2 <- qplot(
-  new_time,
-  NumCells,
-  group = interaction(seed, K, mig_rate),
-  data = data,
-  colour = div0,
-  geom = "line")
-qp2 + scale_colour_gradientn(colours=c(brewer.pal(5,"RdYlBu"), "#0000FF"), name = "diversity at periphery") +
-  facet_grid(.~K, scales= "free") + 
-  #scale_x_continuous(limits = c(0, 520), name = "time (years)", labels = 0:5) +
-  scale_y_continuous(name = "tumour size") +
-  theme_classic()
+#' Plot growth trajectories, coloured by driver edge diversity
+#' 
+#' @param df data frame
+#' @param output_dir folder in which to save the image file; if NA then plots are displayed on screen instead
+#' @param output_filename name of output image file
+#' @param file_type either "pdf" or "png" (other values default to "pdf")
+#' 
+#' @return a plot object
+#' 
+#' @export
+#' @import ggplot2
+#' @import RColorBrewer
+plot_trajectories_by_diversity <- function(df, output_filename = NA, file_type = "png", output_dir = NA) {
+  if(!is.na(output_filename)) if(substr(output_dir, nchar(output_dir), nchar(output_dir)) != "/") output_dir <- paste0(output_dir, "/")
+  
+  if(!is.na(output_dir)) {
+    if(file_type == "png") png(paste0(output_dir, output_filename, ".png"), width = 1000, height = 1000, res = 100)
+    else pdf(paste0(output_dir, output_filename, ".pdf"), width = 500, height = 600)
+  }
+  
+  qp2 <- qplot(
+    new_time,
+    NumCells,
+    group = interaction(seed, K),
+    data = df,
+    colour = div0,
+    geom = "line")
+  qp2 + scale_colour_gradientn(colours = c(brewer.pal(5, "RdYlBu"), "#0000FF"), name = "diversity at periphery") +
+    facet_grid(.~K, scales = "free") +
+    #scale_x_continuous(limits = c(0, 520), name = "time (years)", labels = 0:5) +
+    scale_y_continuous(name = "tumour size") +
+    theme_classic()
+}
 
 # correlations between waiting time and diversity measured at different depths, coloured by size at measurement (JohnCode1)
 
